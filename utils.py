ba7ce8e1a8cd547a6b3d58d7aca550f97e7c8a62
Giorgi Kvernadze <colonialjelly@gmail.com>
2021-02-14 00:51:20 -0700

    rename var and rearrange order

--- utils.py
+++ utils.py
@@ -7,30 +7,29 @@ def get_candidates_batch(X_input_names: np.ndarray,
                          X_source_names: np.ndarray,
                          names: np.ndarray,
                          num_candidates: int = 10,
-                         dist_type='dot_product'):
+                         metric='cosine'):
     """
-    A function that computes scores according to a given distance/similarity function between the input batch of names
-    and the all of the source names.
+    A function that computes scores between the input names and the source names using the the given metric type.
 
     :param X_input_names: Vectorized input names of shape (m, k) where m is the number of input names and
                           k is the dimensionality of each vectorized name.
     :param X_source_names: Vectorized source names of shape (n, k)
     :param names: a nd.array that contains the actual string value of names
     :param num_candidates: Number of candidates to retrieve per name
-    :param dist_type: Type of distance measurement
+    :param metric: Type of metric to use for fetching candidates
     :return: candidates: an nd.array of shape (m, num_candidates, 2)
     """
     sorted_scores_idx = None
 
-    if dist_type == 'dot_product':
+    if metric == 'dot_product':
         scores = safe_sparse_dot(X_input_names, X_source_names.T)
-    elif dist_type == 'cosine':
+    elif metric == 'cosine':
         scores = cosine_similarity(X_input_names, X_source_names)
-    elif dist_type == 'euclidean':
+    elif metric == 'euclidean':
         scores = euclidean_distances(X_input_names, X_source_names)
         sorted_scores_idx = np.argsort(scores, axis=1)[:, 1:num_candidates+1]
     else:
-        raise ValueError("Unrecognized similarity/distance type. Valid options: 'dot_product', 'cosine', 'euclidean'")
+        raise ValueError("Unrecognized metric type. Valid options: 'dot_product', 'cosine', 'euclidean'")
 
     if sorted_scores_idx is None:
         sorted_scores_idx = np.flip(np.argsort(scores, axis=1), axis=1)[:, 1:num_candidates+1]
